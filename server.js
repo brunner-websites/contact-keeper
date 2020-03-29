5983980921010f8a218e772dcb6deff535293a44
sandro <sandro@brunner-websites.com>
2020-03-29 18:07:30 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,14 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production (react)
+if (process.env.node_env === 'production') {
+  // Set static folder (static react assets)
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
+}
 
 const PORT = process.env.PORT || 5000;
 
